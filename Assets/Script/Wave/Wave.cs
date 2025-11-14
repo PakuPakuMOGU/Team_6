78f81c46dfe1d7fcdf10effcb71a9eb417a12fbd
pakupaku <kof2464706@o-hara.stu.ac.jp>
2025-11-14 14:28:09 +0900

    消えすぎ消えすぎ

---
+++ Assets/Script/Wave/Wave.cs
@@ -0,0 +1,86 @@
+using UnityEngine;
+
+public class Wave : MonoBehaviour
+{
+    [Header("フレームレート設定")]
+    [SerializeField] private int targetFPS = 60;
+
+    [Header("ウェーブ時間（秒）")]
+    [SerializeField] private int wave1Duration = 120;
+    [SerializeField] private int wave2Duration = 120;
+    [SerializeField] private int wave3Duration = 120;
+    [SerializeField] private int intervalBetweenWaves = 60;
+
+    [Header("ゲーム終了処理")]
+    public Sphere sphere;
+
+    private enum WaveState { Wave1, Interval1, Wave2, Interval2, Wave3, Finished }
+    private WaveState currentState = WaveState.Wave1;
+
+    private int frameCounter = 0;
+
+    void Awake()
+    {
+        QualitySettings.vSyncCount = 0;
+        Application.targetFrameRate = targetFPS;
+    }
+
+    void Start()
+    {
+        // フレーム換算.
+        wave1Duration *= targetFPS;
+        wave2Duration *= targetFPS;
+        wave3Duration *= targetFPS;
+        intervalBetweenWaves *= targetFPS;
+    }
+
+    void Update()
+    {
+        frameCounter++;
+
+        switch (currentState)
+        {
+            case WaveState.Wave1:
+                if (frameCounter >= wave1Duration)
+                    TransitionTo(WaveState.Interval1);
+                break;
+
+            case WaveState.Interval1:
+                if (frameCounter >= intervalBetweenWaves)
+                    TransitionTo(WaveState.Wave2);
+                break;
+
+            case WaveState.Wave2:
+                if (frameCounter >= wave2Duration)
+                    TransitionTo(WaveState.Interval2);
+                break;
+
+            case WaveState.Interval2:
+                if (frameCounter >= intervalBetweenWaves)
+                    TransitionTo(WaveState.Wave3);
+                break;
+
+            case WaveState.Wave3:
+                if (frameCounter >= wave3Duration)
+                    EndGame();
+                break;
+
+            case WaveState.Finished:
+                break;
+        }
+    }
+
+    private void TransitionTo(WaveState nextState)
+    {
+        currentState = nextState;
+        frameCounter = 0;
+        Debug.Log($"状態遷移: {nextState}");
+    }
+
+    private void EndGame()
+    {
+        currentState = WaveState.Finished;
+        sphere.GameFinish();
+        Debug.Log("ゲーム終了！");
+    }
+}
