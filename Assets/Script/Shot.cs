72db0855a072a49b8dbf15411197be8df0b42893
望月 柚菜 <kof2464737@stu.o-hara.ac.jp>
2025-11-21 10:14:38 +0900

    銃できた

--- Assets/Script/Shot.cs
+++ Assets/Script/Shot.cs
@@ -5,8 +5,9 @@ public class Shot : MonoBehaviour
 {
     public UnityEngine.Camera mainCam;
 
+
     public AudioClip gunshotClip; // 銃声
-    public AudioClip echoClip;    // 2秒後に鳴らす音（例：反響音、リロード音など）
+    public AudioClip echoClip;    // 反響音など
 
     private AudioSource audioSource;
     private Animator animator;
@@ -23,13 +24,7 @@ public class Shot : MonoBehaviour
 
         audioSource.playOnAwake = false;
         audioSource.volume = 1.0f;
-        audioSource.spatialBlend = 0f; // 2Dサウンドにする
-
-        if (gunshotClip != null)
-        {
-            Debug.Log("再生テスト");
-            audioSource.PlayOneShot(gunshotClip);
-        }
+        audioSource.spatialBlend = 0f; // 2Dサウンド
     }
 
     void Update()
@@ -44,18 +39,14 @@ public class Shot : MonoBehaviour
 
             Debug.Log("ばーん");
 
-            // 🎬 アニメーション再生
-            if (animator != null)
-            {
-                animator.SetTrigger("Shot");
-                animator.ResetTrigger("Shot");
+            // アニメーション再生
+            animator.SetBool("Shot", true);
+            StartCoroutine(ResetShootFlag());
 
-            }
-
-            // ⏱ 2秒後に別の音を再生
+            // ⏱ 反響音を2秒後に再生
             if (echoClip != null)
             {
-                StartCoroutine(PlayDelayedSound(1f, echoClip));
+                StartCoroutine(PlayDelayedSound(2f, echoClip));
             }
 
             // 画面中央からRayを飛ばす
@@ -70,11 +61,16 @@ public class Shot : MonoBehaviour
         }
     }
 
-
-
     IEnumerator PlayDelayedSound(float delay, AudioClip clip)
     {
         yield return new WaitForSeconds(delay);
         audioSource.PlayOneShot(clip);
     }
+
+    IEnumerator ResetShootFlag()
+    {
+        yield return null; // 1フレーム後にfalse
+        animator.SetBool("Shot", false);
+    }
 }
+
