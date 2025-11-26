b1fa9122a9ab977afb6c6cc711ebc492dcad9a57
望月 柚菜 <kof2464737@stu.o-hara.ac.jp>
2025-11-26 11:57:55 +0900

    こわいから一回ｾｰﾌﾞ

--- Assets/Script/PlayerA/Shop_Maneger.cs
+++ Assets/Script/PlayerA/Shop_Maneger.cs
@@ -4,21 +4,72 @@ using UnityEngine;
 
 public class Shop_Maneger : MonoBehaviour
 {
+    private int targetIndex = 0;
+
+    [Header("罠の在庫")]
+    [SerializeField] private Transform[] targets;
+
+    private Vector3 originalPosition; // 元の位置を保存
+    private bool hasSaved = false;    // 保存済みかどうか
+
+    public GameObject CancelButton;
+
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     public void BuyKagu()
     {
+        if (targetIndex >= targets.Length)
+        {
+            return;//置くものない
+        }
+
+        if (!hasSaved)
+        {
+            originalPosition = targets[targetIndex].position;
+            hasSaved = true;
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
     }
+
+
+    public void Cancel(GameObject target)
+    {
+
+
+        if (hasSaved)
+        {
+            target.transform.position = originalPosition;
+            hasSaved = false; // 一度戻したらリセット
+        }
+
+
+
+    }
+
 }
