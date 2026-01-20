9ace75678e7d388401cb969f16a99c2a2115a8ee
望月 柚菜 <kof2464737@stu.o-hara.ac.jp>
2026-01-20 10:45:11 +0900

    特殊ロボット完成～ショップリストの更新しなきゃね

--- Assets/Script/PlayerA/Trap/Robotto1Wepon.cs
+++ Assets/Script/PlayerA/Trap/Robotto1Wepon.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
-public class RobottoWeapon : MonoBehaviour
+public class Robotto1Weapon : MonoBehaviour
 {
     [SerializeField] private int damage = 10;
     private void OnTriggerEnter(Collider other)
