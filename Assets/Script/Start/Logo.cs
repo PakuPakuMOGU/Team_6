1ad1d1feca26891fd2b8531493dabb8db5dfaceb
pakupaku <kof2464706@o-hara.stu.ac.jp>
2026-01-26 09:44:42 +0900

    見易く

--- Assets/Script/Start/Logo.cs
+++ Assets/Script/Start/Logo.cs
@@ -4,11 +4,11 @@ using UnityEngine.UI;
 public class Logo : MonoBehaviour
 {
     public Image BackImage;
-    public Image Logoimage;
+    public Image LogoImage;
 
     public float fadeSpeed = 1f;     // フェードインの速さ.
     public float finishSpeed = 2f;   // フェードアウトの速さ.
-    public float waitTime = 1f;      // 完全に表示されてから待つ時間.
+    public float waitTime = 1f;      // 完全に表示されてから消すまでの時間.
 
     private float timer = 0f;
     private enum State { FadeIn, Wait, FadeOut, Finish }
@@ -19,9 +19,10 @@ public class Logo : MonoBehaviour
         // 背景を表示.
         BackImage.gameObject.SetActive(true);
         // ロゴは最初透明.
-        Color c = Logoimage.color;
+        LogoImage.gameObject.SetActive(true);
+        Color c = LogoImage.color;
         c.a = 0f;
-        Logoimage.color = c;
+        LogoImage.color = c;
     }
 
     void Update()
@@ -48,7 +49,7 @@ public class Logo : MonoBehaviour
             case State.Finish:
                 // 完了後の処理（非表示など）
                 BackImage.gameObject.SetActive(false);
-                Logoimage.gameObject.SetActive(false);
+                LogoImage.gameObject.SetActive(false);
                 break;
         }
     }
@@ -56,14 +57,14 @@ public class Logo : MonoBehaviour
     // フェードイン.
     void FadeIn()
     {
-        Color c = Logoimage.color;
+        Color c = LogoImage.color;
         c.a += fadeSpeed * Time.deltaTime;
-        Logoimage.color = c;
+        LogoImage.color = c;
 
         if (c.a >= 1f)
         {
             c.a = 1f;
-            Logoimage.color = c;
+            LogoImage.color = c;
             timer = 0f;
             state = State.Wait;
         }
@@ -72,14 +73,14 @@ public class Logo : MonoBehaviour
     // フェードアウト.
     void FadeOut()
     {
-        Color c = Logoimage.color;
+        Color c = LogoImage.color;
         c.a -= finishSpeed * Time.deltaTime;
-        Logoimage.color = c;
+        LogoImage.color = c;
 
         if (c.a <= 0f)
         {
             c.a = 0f;
-            Logoimage.color = c;
+            LogoImage.color = c;
             state = State.Finish;
         }
     }
