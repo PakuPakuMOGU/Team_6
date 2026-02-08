8e73642b75739cea059936ddc78333fe68712946
PakuPakuMOGU <kof2464706@stu.o-hara.ac.jp>
2026-02-08 22:52:25 +0900

    多分クリアできるようなった

--- Assets/Script/Point.cs
+++ Assets/Script/Point.cs
@@ -6,7 +6,9 @@ using TMPro;
 public class Point : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI textMeshPro;
+    [SerializeField] private int SphereNum = 19;
     [SerializeField] private AudioSource Sound;
+    [SerializeField] private Wave wave;
     private int point = 0;
 
     void Start()
@@ -14,6 +16,7 @@ public class Point : MonoBehaviour
         textMeshPro.text = point.ToString();
     }
 
+    // ポイント追加.
     public bool Add(int add)
     {
         if(point < -add) return false;
@@ -22,5 +25,15 @@ public class Point : MonoBehaviour
         textMeshPro.text = point.ToString();
         Sound.Play();
         return true;
+    }
+
+    // スフィアの個数把握.
+    public void SphereBreak()
+    {
+        SphereNum--;
+
+        // すべて壊したらクリア.
+        if (SphereNum <= 0)
+            wave.EndGame_B();
     }
 }
